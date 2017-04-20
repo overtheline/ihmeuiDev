727a41e36f3f894b0a5f10a204191810cf5f394b
Aaron Keel <akeel@uw.edu>
2017-04-20 13:31:10 -0700

    implement map chart component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { assign } from 'lodash';
+import { assign, bindAll, map } from 'lodash';
+import { json } from 'd3';
 import styles from './App.css';
 // import printFib from './Fib';
 import FAData from '../../../ihme-ui/src/test-utils/data2';
@@ -8,17 +9,19 @@ import colorScale from '../utils/color';
 
 import LineChart from './LineChart';
 import ScatterChart from './ScatterChart';
+import MapChart from './MapChart';
 import Controls from './Controls';
 
-import dataConfig from '../constants/dataConfig';
+import { lineDataConfig, mapDataConfig } from '../constants/dataConfig';
 import { locationList, yearList } from '../constants/metadata';
 
-import { lineDataReducer, lineDomainReducer, lineRangeReducer } from '../reducers/lineData';
+import { lineDataReducer, lineDomainReducer, lineRangeReducer, mapDomainReducer } from '../reducers/data';
 
 // development components
 // import Timer from '../../../ihme-ui/src/ui/animate/src/utils/Timer';
 
-const dataGenerator = new FAData(dataConfig);
+const lineDataGenerator = new FAData(lineDataConfig);
+const mapDataGenerator = new FAData(mapDataConfig);
 
 class App extends React.Component {
   constructor(props) {
@@ -36,19 +39,53 @@ class App extends React.Component {
       },
       lineData: [],
       lineDomain: [],
+      mapSettings: {
+        measure: 'A',
+        risk: 123,
+        type: 1,
+        location: [],
+        year: 2000,
+      },
+      mapData: [{}],
+      mapDomain: [0, 1],
+      selectedChoroplethDomain: [0, 1],
     };
 
-    this.onChangeMeasure = this.onChangeMeasure.bind(this);
+    bindAll(this, [
+      'onResetScale',
+      'onSliderMove',
+      'onChangeMeasure',
+    ]);
   }
 
   componentWillMount() {
-    const initialData = dataGenerator.getData(this.state.lineSettings);
+    const initialLineData = lineDataGenerator.getData(this.state.lineSettings);
+    this.cache.set(initialLineData);
+    this.state.xDomain = lineDomainReducer(initialLineData);
+    this.state.yDomain = lineRangeReducer(initialLineData);
+    this.state.lineData = lineDataReducer(initialLineData);
+  }
 
-    this.cache.set(initialData);
+  componentDidMount() {
+    json('world.topo.json', (error, topology) => {
+      if (error) throw error;
 
-    this.state.xDomain = lineDomainReducer(initialData);
-    this.state.yDomain = lineRangeReducer(initialData);
-    this.state.lineData = lineDataReducer(initialData);
+      this.setState({ topology });
+
+      const choroplethLocationList = map(
+        topology.objects.national.geometries,
+        geo => Number(geo.properties.loc_id),
+      );
+      const mapSettings = assign({}, this.state.mapSettings, { location: choroplethLocationList });
+      const mapData = mapDataGenerator.getData(mapSettings);
+
+      this.cache.set(mapData);
+      this.setState({
+        mapSettings,
+        mapData,
+        mapDomain: mapDomainReducer(mapData),
+      });
+    });
   }
 
   onChangeMeasure(_, nextMeasure) {
@@ -58,7 +95,7 @@ class App extends React.Component {
     if (this.cache.has(lineSettings)) {
       nextData = this.cache.get(lineSettings);
     } else {
-      nextData = dataGenerator.getData(lineSettings);
+      nextData = lineDataGenerator.getData(lineSettings);
       this.cache.set(nextData);
     }
 
@@ -70,15 +107,29 @@ class App extends React.Component {
     });
   }
 
+  onSliderMove(selectedChoroplethDomain) {
+    this.setState({
+      selectedChoroplethDomain,
+    });
+  }
+
+  onResetScale() {
+    this.setState({
+      selectedChoroplethDomain: [0, 1],
+    });
+  }
+
   render() {
     return (
       <div className={styles.app}>
         <h1>Charts</h1>
         <div className={styles['charts-container']}>
           <div className={styles.chart}>
+            <p>This chart animates on the path in the line component.</p>
             <LineChart
+              animate={{}}
               width={500}
-              height={300}
+              height={250}
               xDomain={this.state.xDomain}
               yDomain={this.state.yDomain}
               data={this.state.lineData}
@@ -86,15 +137,27 @@ class App extends React.Component {
             />
           </div>
           <div className={styles.chart}>
+            <p>This chart animates on the path in the shape component.</p>
             <ScatterChart
+              animate={{}}
               width={500}
-              height={300}
+              height={250}
               xDomain={this.state.xDomain}
               yDomain={this.state.yDomain}
               data={this.state.lineData}
               colorScale={colorScale}
             />
           </div>
+        </div>
+        <div className={styles['map-chart-container']}>
+          <MapChart
+            topology={this.state.topology}
+            data={this.state.mapData}
+            range={this.state.mapDomain}
+            selectedChoroplethDomain={this.state.selectedChoroplethDomain}
+            onResetScale={this.onResetScale}
+            onSliderMove={this.onSliderMove}
+          />
         </div>
         <Controls
           onChangeMeasure={this.onChangeMeasure}
